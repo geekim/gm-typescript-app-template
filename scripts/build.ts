8944e03e0f00d45deb182b697bfdf6f25071910c
abert <1248824453@qq.com>
2021-07-08 17:08:25 +0800

    chore: 格式化和打印打包后的文件

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,26 +1,28 @@
-import webpack,{Stats} from 'webpack'
+import webpack, { Stats } from 'webpack'
 
 import proConfig from '../config/webpack.config.prod'
 
-
+// eslint-disable-next-line no-console
 console.log('Creating an optimized production build ...')
 
 const complier = webpack(proConfig)
 
-complier.run((err,stats) => {
-  if(err) {
-    console.error(err);
-    return;
+complier.run((err, stats) => {
+  if (err) {
+    console.error(err)
+    return
   }
-  
+
   const option: Stats.ToStringOptions = {
-    modules:true,
-    colors:true,
+    modules: true,
+    colors: true,
     timings: true
   }
   const statsMes = stats.toJson(option)
 
+  // eslint-disable-next-line no-console
   console.log('打包使用时间：', statsMes.time)
 
-  // console.info(statsMes)
+  // eslint-disable-next-line no-console
+  console.info(stats.toString(option))
 })
--- scripts/start.ts
+++ scripts/start.ts
@@ -1,31 +1,30 @@
 import webpack from 'webpack'
-import WebpackDevServer from 'webpack-dev-server';
+import WebpackDevServer from 'webpack-dev-server'
 
 import devConfig from '../config/webpack.config.dev'
-import {appBuild} from '../constant/paths'
+import { appBuild } from '../constant/paths'
 // 代理
 import proxy from '../constant/proxy'
 // 端口号
-const PORT:number = 3000;
+const PORT = 3000
 
 const DevServerOptions: WebpackDevServer.Configuration = {
-  contentBase:appBuild,
-  hot:true,
-  open:true,
-  host:'localhost',
-  port:PORT,
-  historyApiFallback:{
-    disableDotRule:true,
+  contentBase: appBuild,
+  hot: true,
+  open: true,
+  host: 'localhost',
+  port: PORT,
+  historyApiFallback: {
+    disableDotRule: true
   },
-  inline:true,
-  stats:'errors-only',
-  clientLogLevel:'none',
-  proxy,
+  inline: true,
+  stats: 'errors-only',
+  clientLogLevel: 'none',
+  proxy
 }
 
-const complier = webpack(devConfig);
+const complier = webpack(devConfig)
 
-const server = new WebpackDevServer(complier,DevServerOptions)
-
+const server = new WebpackDevServer(complier, DevServerOptions)
 
 server.listen(PORT)
